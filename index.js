7e691fec74e85154c6f2d6c2662d12325acd02ab
Mohammod Ali <mohammodali651@gmail.com>
2025-06-24 02:02:06 +0600

    specific job data loader function added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -39,6 +39,14 @@ async function run() {
       const cursor = jobsCollection.find()
       const result = await cursor.toArray()
       res.send(result)
+    })
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+
     })
 
 
