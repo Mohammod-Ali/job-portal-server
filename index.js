b9e7d703a2fbbb3f8807bf6964c60217a6ff5e5f
Mohammod Ali <mohammodali651@gmail.com>
2025-06-28 22:30:03 +0600

    job post api added

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/jobs', async(req, res) => {
+      const newJob = req.body
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
+    })
+
     // job applications APIs
     // get all data, get one data, get some data [0,1,many]
     app.get('/job-application', async(req, res) => {
