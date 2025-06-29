f6a2f19e8e5819ce6d3c45d14d2308e389adf28d
Mohammod Ali <mohammodali651@gmail.com>
2025-06-29 18:24:53 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
+const logger = (req, res, next) => {
+  console.log('insite the logger')
+  next()
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized Access'})
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized Access'})
+    }
+    
+  })
+
+  next()
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kkwzhai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,6 +56,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // auth related APIs
+    app.post('/jwt', async(req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+
+      })
+      .send(token)
+    })
+    
 
 
     // jobs related APIs
@@ -65,9 +105,12 @@ async function run() {
 
     // job applications APIs
     // get all data, get one data, get some data [0,1,many]
-    app.get('/job-application', async(req, res) => {
+    app.get('/job-application', verifyToken, async(req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email}
+
+      console.log('from jwt', req.cookies)
+
       const result = await jobApplicationCollection.find(query).toArray()
 
       // not the best way to aggregate data
