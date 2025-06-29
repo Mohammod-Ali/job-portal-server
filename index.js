e2b8d5df35c0c1c22ac6026003d6c7c567aa68e1
Mohammod Ali <mohammodali651@gmail.com>
2025-06-29 16:47:39 +0600

    job applications api added

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ async function run() {
 
       // not the best way to aggregate data
       for( const application of result) {
-        console.log(application.job_id)
         const query1 = {_id: new ObjectId(application.job_id)}
         const job = await jobsCollection.findOne(query1)
         if(job){
@@ -86,13 +85,55 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/job-applications/jobs/:job_id', async(req, res) => {
+      const jobId = req.params.job_id
+      console.log(jobId)
+      const query = {job_id: jobId}
+      const result = await jobApplicationCollection.find(query).toArray()
+      res.send(result)
+      console.log(result)
+    })
+
 
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application)
+      // not the best way for use aggregate
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id)}
+      const job = await jobsCollection.findOne(query)
+      let newCount = 0
+      if(job.applicationCount) {
+        newCount  = job.applicationCount +1
+      }else{
+        newCount = 1
+      }
+
+      // now update the job info
+      const filter = { _id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount
+        }
+      }
+
+      const updateResult = await jobsCollection.updateOne(filter, updatedDoc)
+
       res.send(result)
     })
 
+    app.patch('/job-applications/:id', async(req, res) => {
+      const id = req.params.id
+      const data = req.body
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          status: data.status
+        }
+      }
+      const result = await jobApplicationCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
 
 
 
